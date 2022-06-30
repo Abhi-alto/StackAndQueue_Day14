fbc99e4937aa50943dc54a65053a55e710890389
unknown <abhisheksrivastava9719@gmail.com>
2022-06-30 08:05:19 +0530

    Created a Stack

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Welcome to stacks and Queue");
+﻿using Stack_ad_Queue;
+Console.WriteLine("Welcome to stacks and Queue");
+StackANDQueue datatype= new StackANDQueue();
+datatype.push(70);
+datatype.push(30);
+datatype.push(56);
+datatype.print();
---
+++ StackANDQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_ad_Queue
+{
+    internal class StackANDQueue
+    {
+        Node top;
+        public StackANDQueue()
+        {
+            this.top = null;
+        }
+        public void push(int pushNode)
+        {
+            Node node = new Node(pushNode);
+            if (top == null)
+            {
+                node.next = null; ;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+            Console.WriteLine("Pushed nodewith data "+node.data);
+        }
+        public void print()
+        {
+            Node temp = top;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
