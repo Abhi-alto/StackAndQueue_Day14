ee4ebf1c710e3e614ce469e139b50ff86374e50e
unknown <abhisheksrivastava9719@gmail.com>
2022-06-30 16:55:59 +0530

    Queue created

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 ﻿using Stack_ad_Queue;
 Console.WriteLine("Welcome to stacks and Queue");
 StackANDQueue datatype= new StackANDQueue();
-datatype.push(70);
-datatype.push(30);
-datatype.push(56);
-datatype.print();
-datatype.fullpop();
-/*datatype.print();
-datatype.pop();
-datatype.print();*/
+Queue que = new Queue();
+que.Enqueue(56);
+que.Enqueue(30);
+que.Enqueue(70);
+que.display();
+
--- Queue.cs
+++ Queue.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Stack_ad_Queue
 {
-    internal class Queue
+    public class Queue
     {
+        Node rear ;
+        Node front ;
+        public Queue()
+        {
+            this.rear = null;
+            this.front = null;
+        }
+        public void Enqueue(int AddToQueue)
+        {
+            Node node = new Node(AddToQueue);
+            if(front==null)
+            {
+                rear = node;
+                front = rear;
+                Console.WriteLine("Added the node " + rear.data + " to the queue");
+            }
+            else
+            {
+                rear.next = node;
+                rear = rear.next;
+                Console.WriteLine("Added the node " + rear.data + " to the queue");
+            }
+        }
+        public void display()
+        {
+            if(rear==null&&front ==null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = front;
+                while(temp!=null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
     }
 }
