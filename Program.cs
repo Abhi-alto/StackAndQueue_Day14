b6768e7f7367735eee002414242119944c3c6854
unknown <abhisheksrivastava9719@gmail.com>
2022-06-30 17:03:15 +0530

    Welcome to stack and ques

--- Program.cs
+++ Program.cs
@@ -1,20 +1,9 @@
 ﻿using Stack_ad_Queue;
 Console.WriteLine("Welcome to stacks and Queue");
 StackANDQueue datatype= new StackANDQueue();
-<<<<<<< HEAD
-datatype.push(70);
-datatype.push(30);
-datatype.push(56);
-datatype.print();
-datatype.peek();
-datatype.print();
-datatype.pop();
-datatype.print();
-=======
 Queue que = new Queue();
 que.Enqueue(56);
 que.Enqueue(30);
 que.Enqueue(70);
 que.display();
 
->>>>>>> CreateQueue
